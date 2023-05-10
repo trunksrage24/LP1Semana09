880474e41b790afb2db53d9c7af2c3c640488a52
Vitor <vitor.daniel@outlook.pt>
2023-05-10 19:17:08 +0100

    create Dog

---
+++ AnimalPerspective/Dog.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalPerspective
+{
+    public class Dogg : Animal
+    {
+        public override string Sound()
+        {
+            return "Woof!";
+        }
+    }
+
+}
