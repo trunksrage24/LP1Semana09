67deb7afc978aec00f749addb15df5d0afce8127
Vitor <vitor.daniel@outlook.pt>
2023-05-10 19:35:57 +0100

    Implement IMammal to Cat

--- AnimalPerspective/IMammal.cs
+++ AnimalPerspective/IMammal.cs
@@ -9,12 +9,22 @@ namespace AnimalPerspective
     {
         int NumberOfNipples { get; }
     }
+ 
     public class Dog : IMammal
     {
         public int NumberOfNipples { get; }
         public Dog()
         {
             NumberOfNipples = 8; // Assuming dogs have 8 nipples
+        }
+    }
+
+    public class Cat : IMammal
+    {
+        public int NumberOfNipples { get; }
+        public Cat()
+        {
+            NumberOfNipples = 6; // Assuming cats have 6 nipples
         }
     }
 }
