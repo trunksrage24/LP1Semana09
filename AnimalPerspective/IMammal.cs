5436a514e43ea40d3c97c74cf9d57f248c3df813
Vitor <vitor.daniel@outlook.pt>
2023-05-10 19:33:19 +0100

    create IMammal

---
+++ AnimalPerspective/IMammal.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalPerspective
+{
+    public interface IMammal
+    {
+        int NumberOfNipples { get; }
+    }
+}
