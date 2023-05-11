424bd765b7588913f1a0fdd53a406cf6d2152689
Vitor <vitor.daniel@outlook.pt>
2023-05-11 15:18:13 +0100

    create Player class and download Program

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager1
+{
+    public class Player
+    {
+        
+    }
+}
--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -1,12 +1,144 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace PlayerManager1
+namespace PlayerManager1 // >>> Change to PlayerManager2 for exercise 4 <<< //
 {
-    class Program
+    /// <summary>
+    /// The player listing program.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The list of all players.
+        /// </summary>
+        private List<Player> playerList;
+
+        /// <summary>
+        /// Program begins here.
+        /// </summary>
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the player listing program
+            Program prog = new Program();
+            // Start the program instance
+            prog.Start();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the player listing program.
+        /// </summary>
+        private Program()
+        {
+            // Initialize the player list with two players using collection
+            // initialization syntax
+            playerList = new List<Player>() {
+                new Player("Best player ever", 100),
+                new Player("An even better player", 500)
+            };
+        }
+
+        /// <summary>
+        /// Start the player listing program instance
+        /// </summary>
+        private void Start()
+        {
+            // We keep the user's option here
+            string option;
+
+            // Main program loop
+            do
+            {
+                // Show menu and get user option
+                ShowMenu();
+                option = Console.ReadLine();
+
+                // Determine the option specified by the user and act on it
+                switch (option)
+                {
+                    case "1":
+                        InsertPlayer();
+                        break;
+                    case "2":
+                        ListPlayers(playerList);
+                        break;
+                    case "3":
+                        ListPlayersWithScoreGreaterThan();
+                        break;
+                    case "4":
+                        Console.WriteLine("Bye!");
+                        break;
+                    default:
+                        Console.Error.WriteLine("\n>>> Unknown option! <<<\n");
+                        break;
+                }
+
+                // Wait for user to press a key...
+                Console.Write("\nPress any key to continue...");
+                Console.ReadKey(true);
+                Console.WriteLine("\n");
+
+                // Loop keeps going until players choses to quit (option 4)
+            } while (option != "4");
+        }
+
+        /// <summary>
+        /// Shows the main menu.
+        /// </summary>
+        private void ShowMenu()
+        {
+            // /////////////////// //
+            // COMPLETE ME PLEASE! //
+            // /////////////////// //
+        }
+
+        /// <summary>
+        /// Inserts a new player in the player list.
+        /// </summary>
+        private void InsertPlayer()
+        {
+            // /////////////////// //
+            // COMPLETE ME PLEASE! //
+            // /////////////////// //
+        }
+
+        /// <summary>
+        /// Show all players in a list of players. This method can be static
+        /// because it doesn't depend on anything associated with an instance
+        /// of the program. Namely, the list of players is given as a parameter
+        /// to this method.
+        /// </summary>
+        /// <param name="playersToList">
+        /// An enumerable object of players to show.
+        /// </param>
+        private static void ListPlayers(IEnumerable<Player> playersToList)
+        {
+            // /////////////////// //
+            // COMPLETE ME PLEASE! //
+            // /////////////////// //
+        }
+
+        /// <summary>
+        /// Show all players with a score higher than a user-specified value.
+        /// </summary>
+        private void ListPlayersWithScoreGreaterThan()
+        {
+            // /////////////////// //
+            // COMPLETE ME PLEASE! //
+            // /////////////////// //
+        }
+
+        /// <summary>
+        /// Get players with a score higher than a given value.
+        /// </summary>
+        /// <param name="minScore">Minimum score players should have.</param>
+        /// <returns>
+        /// An enumerable of players with a score higher than the given value.
+        /// </returns>
+        private IEnumerable<Player> GetPlayersWithScoreGreaterThan(int minScore)
+        {
+            // /////////////////// //
+            // COMPLETE ME PLEASE! //
+            // /////////////////// //
         }
     }
 }
