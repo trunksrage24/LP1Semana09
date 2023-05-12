fa2c17e943ed5b7dd115a21970fba8909e635ed5
Vitor <vitor.daniel@outlook.pt>
2023-05-12 16:58:07 +0100

    create Player class

--- PlayerManager1/Player.cs
+++ PlayerManager1/Player.cs
@@ -1,12 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace PlayerManager1
 {
     public class Player
     {
-        
+        public string Name { get; }
+        public int Score { get; }
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
     }
 }
